7d6424203ac8c666773375079aef6985df1051c0
hilquiasfmelo@hotmail.com <hilquiasfmelo>
2021-09-26 16:57:22 -0300

    :tada: Challenge Finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -19,7 +19,7 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
+export interface MovieProps {
   imdbID: string;
   Title: string;
   Poster: string;
@@ -29,6 +29,14 @@ interface MovieProps {
   }>;
   Runtime: string;
 }
+
+interface SelectedMovie {
+  handleClickButton: (id: number) => void;
+  genres: GenreResponseProps[]; 
+  selectedGenreId: number;
+}
+
+export const SelectedCategoryContext = createContext({} as SelectedMovie);
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
@@ -60,36 +68,10 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <SelectedCategoryContext.Provider value={{genres, handleClickButton, selectedGenreId}}>
+        <SideBar />
+        <Content title={selectedGenre.title} movies={movies} />
+      </SelectedCategoryContext.Provider>
     </div>
   )
 }
